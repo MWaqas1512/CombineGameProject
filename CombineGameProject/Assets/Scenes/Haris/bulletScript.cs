a842c151e3fdae0f70de79a5d9843e4b58bbd5f9
haris-584 <hariskhattak584@gmail.com>
2020-06-13 08:33:23 +0500

    EnemyFire added

---
+++ CombineGameProject/Assets/Scenes/Haris/bulletScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+
+    public Health enemyHealth = null;
+
+    int damage = 50;
+
+
+    void Awake()
+    {
+        enemyHealth = GameObject.FindGameObjectWithTag("enemy").GetComponent<Health>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "enemy")
+        {
+            enemyHealth.HealthPoints -= damage;
+            //  enemyHealth -= damage;
+
+            if (enemyHealth.HealthPoints == 0)
+            {
+                Destroy(collision.gameObject);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ CombineGameProject/Assets/Scenes/Haris/shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    float bulletSpeed = 3100;
+    public GameObject Bullet;
+    AudioSource bulletAudio;
+
+    void Start()
+    {
+        bulletAudio = GetComponent<AudioSource>();
+    }
+
+    void Fire()
+    {
+        //shoot
+        GameObject tempBullet = Instantiate(Bullet, transform.position, transform.rotation) as GameObject;
+        Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
+        tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+        Destroy(tempBullet, 3f);
+
+        //play audio
+        bulletAudio.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+
+    }
+}
